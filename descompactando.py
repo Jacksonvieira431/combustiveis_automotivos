afb6dcf76dba73f23ab1c97ab7c55384ced6c092
Jacksonvieira431 <jacksonhudson431@gmail.com>
2023-08-06 15:08:17 -0300

    atualização de leitura de arquivos

--- descompactando.py
+++ descompactando.py
@@ -1,14 +1,26 @@
 import patoolib
 import os
+from tqdm.auto import tqdm
 
-rar_files = ["Combustíveis Automotivos-2004-a-2022-1.rar", "Combustíveis Automotivos-2004-a-2022-2.rar"]
+# O diretório onde os arquivos do repositório clonado estão
+Diretorio_local = "C:/Users/jacks/OneDrive/Projetos/combustiveis_automotivos"
+
+dest_dir = f"{Diretorio_local}/arquivos-extraidos/"
+
+rar_files = [f"{Diretorio_local}/Combustíveis Automotivos-2012-a-2022.rar"]
+
+for rar_file in rar_files:
+    if not os.path.isfile(rar_file):
+        print(f"Arquivo não encontrado: {rar_file}")
 
 try:
-    for rar_file in rar_files:
-        patoolib.extract_archive(rar_file)
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
 
-    # Depois de descompactar todos os arquivos RAR, agora você pode procurar os arquivos CSV
-    csv_files = [file for file in os.listdir() if file.endswith(".csv")]
+    for rar_file in tqdm(rar_files, desc="Extraindo arquivos"):
+        patoolib.extract_archive(rar_file, outdir=dest_dir)
+
+    csv_files = [file for file in os.listdir(dest_dir) if file.endswith(".csv")]
 
 except Exception as e:
     print("Erro ao ler os arquivos:", e)
